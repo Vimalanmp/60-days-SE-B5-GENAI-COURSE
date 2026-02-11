96219fd3851e2d89a326e6c3e5dc6582e63c3d96
Vimalanmp <mpmvimalan@gmail.com>
2026-02-11 10:39:30 +0530

    Update playwrightkeyfunction.py

--- playwright/playwrightkeyfunction.py
+++ playwright/playwrightkeyfunction.py
@@ -1,1 +1,19 @@
+from playwright.async_api import async_playwright
+import asyncio
 
+
+async def playwright_function():
+    async with async_playwright() as p:
+        browser = await p.chromium.launch(headless=False)
+        pages = await browser.new_page()
+
+        # navigation
+        await pages.goto("https://www.google.com")
+        
+        await pages.wait_for_timeout(2000)  # 2 seconds
+
+        await browser.close()
+
+
+if __name__ == "__main__":
+    asyncio.run(playwright_function())
